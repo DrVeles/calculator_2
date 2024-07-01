5218c4656d87b79e78505234081987ab9f5d4bdd
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-07-01 19:37:45 +0300

    READY: thin controller ready

--- src/controller/controller.h
+++ src/controller/controller.h
@@ -3,28 +3,33 @@
 
 #include <string>
 
+#include "./../model/model.h"
+
 namespace s21 {
 class controller {
  public:
-  controller();
-  ~controller();
-
   /**
-   * @brief func to send string from front to backend
+   * @brief func to send string from front to backend and calculate result
    * @param input_str - string from frontend
    * @param result - result of backend works
    * @return `0` - success
    * @return `1` - failure
    */
-  int api_to_calculate(const char* input_str, long double* result);
+  int api_to_calculate(const std::string input_str, long double* result) {
+    model m;
+    return m.calculate_str(input_str, result);
+  }
 
   /**
-   * @brief func to validate frontend string
+   * @brief func to validate frontend string to correct math notation
    * @param input_str - string from frontend
    * @return `0` - success
    * @return `1` - failure
    */
-  int api_to_validate(const char* input_str);
+  int api_to_validate(const std::string input_str) {
+    model m;
+    return m.validate_string(input_str);
+  }
 };
 
 };  // namespace s21
